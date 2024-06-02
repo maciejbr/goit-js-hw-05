4ab87bdd3f6b7bba633d2cdc15ea35b81057a819
maciejbr <m.broniarski@gmail.com>
2024-06-02 19:58:02 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter((user) => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
+};
+
 const allUsers = [
   {
     name: "Moore Hensley",
