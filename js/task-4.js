5565317ef9507564d436b455abf4b8a048a4b5fb
maciejbr <m.broniarski@gmail.com>
2024-06-03 17:32:21 +0200

    change allUsers to totalUsers

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ const getTotalBalanceByGender = (users, gender) => {
     .reduce((total, user) => total + user.balance, 0);
 };
 
-const allUsers = [
+const totalUsers = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -44,6 +44,6 @@ const allUsers = [
   },
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(totalUsers, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(totalUsers, "female")); // 8863
